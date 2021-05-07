45218041769d2a2f6345900c87903780fe949335
VengerAndrey <vengerandr@gmail.com>
2021-05-07 21:35:17 +0300

    Add UserController.

---
+++ WebApi/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Domain.Services;
+using Microsoft.AspNetCore.Authorization;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var user = await _userService.Get(id);
+
+            if (user is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user.ToDto());
+        }
+    }
+}
